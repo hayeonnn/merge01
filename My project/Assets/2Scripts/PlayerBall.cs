db4e0bd93d309bcde7664c4deb37169d1f92518e
hayeonnn <83764031+hayeonnn@users.noreply.github.com>
2022-03-13 21:34:56 +0900

    codeupload

---
+++ My project/Assets/2Scripts/PlayerBall.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerBall : MonoBehaviour
+{
+    public float jumpPower;
+    public int itemCount;
+    public GameManager manager;
+    bool isJump;
+    Rigidbody rigid;
+
+    void Awake()
+    {
+        isJump = false;
+        rigid = GetComponent<Rigidbody>();
+    }
+    void Update() {
+        if (Input.GetButtonDown("Jump") && !isJump) {
+            isJump = true;
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+    void OnCollisionEnter(Collision collision) { 
+        if(collision.gameObject.tag == "Floor")
+            isJump = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            itemCount++;
+            GetComponent<AudioSource>().Play();
+            other.gameObject.SetActive(false);
+            manager.GetItem(itemCount);
+        }
+        else if (other.tag == "Finish")
+        {
+            if (itemCount == manager.totalItemCount)
+            {                
+                //Game Clear!
+                if (manager.stage == 1)
+                    SceneManager.LoadScene(0);
+                else
+                    SceneManager.LoadScene(manager.stage + 1);
+            }
+            else {
+                //Restart..
+                SceneManager.LoadScene(manager.stage);
+            }
+            //GameObject.FindGameObjectWithTag // find는 cpu많이잡아먹기때문에 잘 안씀
+        }
+    }
+}
